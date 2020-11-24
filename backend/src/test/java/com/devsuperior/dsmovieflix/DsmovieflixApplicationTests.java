5d4f277112549d10741968245eefda4aca0cc3ca
Francisco de Assis Marcelino <famarcelino@gmail.com>
2020-11-24 14:04:33 -0300

    Create a MovieFlix Project

---
+++ backend/src/test/java/com/devsuperior/dsmovieflix/DsmovieflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
