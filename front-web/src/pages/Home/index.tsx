e566ee1d0f2d1ec486794e66f9f41ccfa4b04e75
Francisco de Assis Marcelino <famarcelino@gmail.com>
2020-12-29 09:13:38 -0300

    Add Login Form to Home Page

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
+import ButtonIcon from '../../core/components/ButtonIcon';
+import './styles.scss';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home-container">
+            <div className="row home-content">
+                <div className="col-6">
+                    <h1 className="home-title">Avalie Filmes</h1>
+                    <p className="home-subtitle">
+                        Faça parte do nosso catálogo de divulgação
+                        e aumente a venda dos seus produtos.
+                    </p>
+                    <MainImage />
+                </div>
+
+                <div className="col-6 login-content card-base">
+                    <h1 className="login-title">login</h1>
+                    <form className="login-form">
+                        <input
+                            type="email"
+                            className="form-control input-base margin-bottom-30"
+                            placeholder="Email"
+                        />
+                        <input
+                            type="password"
+                            className="form-control input-base"
+                            placeholder="Senha"
+                        />
+                        <div className="login-button">
+                            <ButtonIcon text="logar" />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
