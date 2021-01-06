b1a11a69dbc63576db83730eee051d1b26fce5e0
Francisco de Assis Marcelino <famarcelino@gmail.com>
2021-01-06 10:17:02 -0300

    Create a mobile Module

--- front-web/src/pages/Movies/index.tsx
+++ front-web/src/pages/Movies/index.tsx
@@ -28,7 +28,7 @@ const Movies = () => {
     return (
         <div className="movie-container">
             <div className="movie-filter border-radius-10">
-                <h2>Aventura</h2>
+                <h2 className="movie-filter-text">Aventura</h2>
             </div>
             <div className="movies-list">
                 {isLoading ? <MovieCardLoader /> : (
